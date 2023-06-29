b161e91addd553f077d41a99aef01257d2af14ac
TylerMorgan142 <tylermorgan142@gmail.com>
2023-06-29 22:03:40 +0000

    Add addTurn function + tests

--- scripts/game.js
+++ scripts/game.js
@@ -10,6 +10,13 @@ function newGame() {
     game.currentGame = [];
     game.playerMoves = [];
     showScore();
+    addTurn();
+}
+
+function addTurn() {
+    game.playerMoves = [];
+    game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
+    // showTurns();
 }
 
 function showScore () {
@@ -18,5 +25,5 @@ function showScore () {
 
 
 
-module.exports = { game, newGame, showScore };
+module.exports = { game, newGame, showScore, addTurn };
 
