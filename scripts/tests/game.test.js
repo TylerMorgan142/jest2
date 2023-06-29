2f1f4563d78d1497b8537bad1a6e98f80ad8c51c
TylerMorgan142 <tylermorgan142@gmail.com>
2023-06-29 21:53:55 +0000

    Add newGame function + tests

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-const { game } = require("../game");
+const { game, newGame } = require("../game");
 
 
 beforeAll(() => {
@@ -29,4 +29,23 @@ describe("game object contains correct keys", () => {
     test("choices contain correct ids", () => {
         expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
     });
+});
+
+describe("newGame works correctly", () => {
+    beforeAll(() => {
+        game.score =42;
+        game.playerMoves = ["button1", "button2"];
+        game.currentGame = ["button1", "button2"];
+        newGame();
+    });
+    test("should set game score to zero", () => {
+        expect(game.score).toEqual(0);
+    });
+    test("should clear the computer sequence array", () => {
+        expect(game.currentGame.length).toBe(0);
+    });
+    test("should clear the player moves array", () => {
+        expect(game.playerMoves.length).toBe(0);
+    });
+    
 });
