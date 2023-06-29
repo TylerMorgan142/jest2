c80b25e91947113da75e31ba9ba5fe7b4317fced
TylerMorgan142 <tylermorgan142@gmail.com>
2023-06-29 21:58:11 +0000

    Add showScore function and tests

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-const { game, newGame } = require("../game");
+const { game, newGame, showScore } = require("../game");
 
 
 beforeAll(() => {
@@ -36,6 +36,7 @@ describe("newGame works correctly", () => {
         game.score =42;
         game.playerMoves = ["button1", "button2"];
         game.currentGame = ["button1", "button2"];
+        document.getElementById("score").innerText = "42";
         newGame();
     });
     test("should set game score to zero", () => {
@@ -47,5 +48,7 @@ describe("newGame works correctly", () => {
     test("should clear the player moves array", () => {
         expect(game.playerMoves.length).toBe(0);
     });
-    
+    test("should display 0 for the element of id of score", () => {
+        expect(document.getElementById("score").innerText).toEqual(0);
+    });
 });
